ed08b61026269747b2a7ffd6cd3d1c029b3f56ea
Ness Chu <vanessa.chu@mail.mcgill.ca>
2020-06-07 19:07:09 -0400

    Create placeholder html and js for game page.

---
+++ portfolio/src/main/webapp/snake.js
@@ -0,0 +1,10 @@
+function snake(){
+    const c = document.getElementById("snakeCanvas");
+    const ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.font = "36px Arial";
+    ctx.fillText("Under Construction!", 20, 125);
+    //ctx.fillStyle = "white";
+    //ctx.fillRect(0, 0, 500, 250);
+}
